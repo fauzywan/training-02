a8818c5af24e3e9d86b7457271640ab1788aac86
iwan Fauzy <iwanfauzay46@gmail.com>
2025-04-01 16:56:07 +0700

    Video Kedua

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -2,16 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Clicker extends Component
 {
+    public $username="testname";
     public function click()
     {
         dump("Aduh Gantengnya");
     }
+    public function createNewUser(){
+        $count=User::count();
+        User::create([
+            'name'=>'test user',
+            'email'=>'test'.$count.'@gmail.com',
+            'password'=>'123',
+        ]);
+    }
     public function render()
     {
-        return view('livewire.clicker');
+        $title="Test";
+        $users=User::all();
+        return view('livewire.clicker',['title'=>$title,'users'=>$users]);
     }
+
 }
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,4 +1,8 @@
-<div>
 
-    <button wire:click="click()" class="bg-red-500 px-3 py-2 rounded-sm font-bold cursor-pointer active:text-white active:scale-[0.9] transition-all duration-300 delay-150" type="button">Click Me</button>
+<div class="text-white">
+
+    <h1>{{$title}}</h1>
+    <h1>{{$username}}</h1>
+    {{ count($users) }}
+    <button wire:click="createNewUser" class="bg-red-500 px-3 py-2 rounded-sm font-bold cursor-pointer active:text-white active:scale-[0.9] transition-all duration-300 delay-150" type="button">Create New User</button>
 </div>
